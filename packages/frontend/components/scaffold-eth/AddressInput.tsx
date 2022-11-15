b5b904359e692ed786e407de0fc453d62a891b99
mcornell <mattimose@gmail.com>
2022-11-15 11:09:54 -0800

    fucking finally

--- packages/frontend/components/scaffold-eth/AddressInput.tsx
+++ packages/frontend/components/scaffold-eth/AddressInput.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import Blockies from "react-blockies";
 import { useEnsAddress } from "wagmi";
-
+import { getDefaultProvider } from "ethers";
 interface IAddressInput {
   onSuccess?: (arg: string) => void;
   placeholder?: string;
@@ -27,6 +27,11 @@ const AddressInput = ({ placeholder, onSuccess }: IAddressInput) => {
     setAddress(event.target.value);
   };
 
+  const [showData, setShowData] = useState(false);
+    function toggle(){
+      setShowData(!showData);
+    }
+
   useEffect(() => {
     if (!ensData) return;
 
@@ -35,11 +40,12 @@ const AddressInput = ({ placeholder, onSuccess }: IAddressInput) => {
       onSuccess(ensData);
     }
   }, [ensData, onSuccess]);
+ 
 
   return (
     <>
       <div className="form-control">
-        <label className="input-group input-group-sm">
+        <label className="input-group input-group-sm" style={{margin:"auto",}}>
           <input
             type="text"
             placeholder={placeholder}
@@ -47,12 +53,26 @@ const AddressInput = ({ placeholder, onSuccess }: IAddressInput) => {
             value={address || ""}
             onChange={onChangeAddress}
             disabled={isLoading}
+            //Some kinda error here for why it splits n stuff after click, cant figure out
+            style={{backgroundColor:'white', color:'black', position:'static', alignItems:'center'}}
+            
           />
-          <span className="p-0 rounded-md bg-base-100 h-10">
-            <Blockies seed={address?.toLowerCase() as string} size={8} scale={5} />
+          <span className="p-0 rounded-md bg-base-100 h-10" style={{}}>
+            <button style={{ borderTopRightRadius: "10px", padding:"4px", borderBottomRightRadius: "10px", border: "1px solid green", backgroundColor: "limegreen", color:"black"}} onClick={toggle}>Show Covenants Farm</button>
           </span>
         </label>
       </div>
+            <div style={{borderRadius:'10px', border: '1px solid white', padding:'50px', marginLeft: '2vw', marginTop:'4vh', marginBottom:'4vh', display: showData?"inline-block":"none"}}>
+              <div>
+                Data 1
+              </div>
+            </div>
+            <div style={{borderRadius:'10px', border: '1px solid white', padding:'50px', marginLeft: '4vw', marginTop:'4vh', marginBottom:'4vh', display: showData?"inline-block":"none"}}>
+              Data 2
+            </div>
+            <div style={{borderRadius:'10px', border: '1px solid white', padding:'50px', marginLeft: '4vw', marginRight: '2vw', marginTop:'4vh', marginBottom:'4vh', display: showData?"inline-block":"none"}}>
+              Data 3
+            </div>
     </>
   );
 };
